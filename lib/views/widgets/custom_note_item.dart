4b0702f262fd51ad190c0f5f18cb9943a08a3802
MahmoudElkhadem <mahmodelkhadem17@gmail.com>
2024-11-02 02:10:43 +0200

    new note

---
+++ lib/views/widgets/custom_note_item.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class CustomNoteItem extends StatelessWidget {
+   CustomNoteItem({super.key, required this.color, required this.title, required this.subtitle, required this.date});
+  Color color;
+  String title;
+  String subtitle;
+  String date;
+
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+      padding:const EdgeInsets.only(top: 23,bottom: 23,left:5),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(10)
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: Text(title,style:const TextStyle(fontSize: 25,color: Colors.black)),
+            subtitle: Text(subtitle,style: TextStyle(fontSize: 19,color: Colors.black.withOpacity(.5))),
+            trailing: IconButton(onPressed: (){} , icon: const Icon(Icons.delete,color: Colors.black,size: 30,)),
+          ),
+           Padding(
+             padding: const EdgeInsets.only(right: 15),
+             child: Text(date,style: TextStyle(color: Colors.black.withOpacity(.5))),
+           )
+        ],
+      )
+    );
+  }
+}
--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:note/views/widgets/custom_appbar.dart';
+import 'package:note/views/widgets/custom_note_item.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 18),
-      child: const Column(
+    return  Padding(
+      padding:const  EdgeInsets.symmetric(horizontal: 18),
+      child:  Column(
         children:[
-          SizedBox(
+          const SizedBox(
             height: 40,
           ),
-          CustomAppbar()
+          const CustomAppbar(),
+          const SizedBox(height: 15),
+          CustomNoteItem(color:const Color(0xFFFFCC80),date: '19 oct 2002',title: 'Frist Note',subtitle: 'Welcome to my app',),
+          const SizedBox(height: 15),
+          CustomNoteItem(color:const Color.fromARGB(255, 234, 105, 225),date: '2 Nov 2024',title: 'To My Love',subtitle: 'I want to be the reason behind your smile.',)
         ]
       ),
     );
